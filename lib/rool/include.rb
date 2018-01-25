ed343c609c177c6958b586286e0d7b4b9bf85066
Andrew Littlefield <andrew.ray.allan@gmail.com>
2018-01-25 14:13:51 -0700

    removed puts

--- lib/rool/include.rb
+++ lib/rool/include.rb
@@ -2,9 +2,6 @@ module Rool
   class Include < Basic
   	def process(dataset)
       super
-      puts dataset[@data_key].include?(@operand)
-      puts dataset
-      puts @operand
      	if dataset[@data_key].include?(@operand)
      		return true
      	else

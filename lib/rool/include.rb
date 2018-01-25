2b1a6ee5029097cdf7637617a728d2822c2ce40e
Andrew Littlefield <andrew.ray.allan@gmail.com>
2018-01-25 14:13:26 -0700

    Finally got include working after realizing stupid mistake

--- lib/rool/include.rb
+++ lib/rool/include.rb
@@ -1,4 +1,15 @@
 module Rool
   class Include < Basic
+  	def process(dataset)
+      super
+      puts dataset[@data_key].include?(@operand)
+      puts dataset
+      puts @operand
+     	if dataset[@data_key].include?(@operand)
+     		return true
+     	else
+     		return false
+     	end
+	  end
   end
 end
